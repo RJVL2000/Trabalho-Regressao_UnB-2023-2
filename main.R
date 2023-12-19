55945aab6d55898b77cf05b10564f5d319f8af6f
Rafael Lira <rafaelvazlira@gmail.com>
2023-12-18 23:18:47 -0300

    Correlação + boxplots + comentários

--- main.R
+++ main.R
@@ -1,20 +1,37 @@
-pacman::p_load(readxl, tidyverse)
+pacman::p_load(readxl, tidyverse, ggplot2)
 
+# Importação de dados e renomeação de variáveis
 dados <- read_excel("./Dados/Dados_trabalho_20232.xlsx")
-# dados <- read_excel("./Trabalho/Trabalho-Regressao_UnB-2023-2//Dados/Dados_trabalho_20232.xlsx")
 
 names(dados) <- c("id", "tempo_internacao", "idade", "prob_infeccao", "prop_culturas_rotina", "prop_raio_x_torax_rotina", "quant_leitos", "filiacao_escola_medicina", "regiao", "media_pacientes", "quant_enfermeiros", "servicos_disponiveis")
 
-dados <- dados %>%
+# Tratamento de variáveis categóricas
+dados_trat <- dados %>%
     mutate(
         filiacao_escola_medicina = filiacao_escola_medicina == 1,
         regiao_ne = regiao == 1,
         regiao_nc = regiao == 2,
-        regiao_s = regiao == 3
+        regiao_s = regiao == 3,
+        regiao = NULL
     )
 
-y_1 <- dados[c("tempo_internacao")]
+# Separação em variáveis respostas e explicativas
+y_1 <- dados_trat[c("tempo_internacao")]
 
-y_2 <- dados[c("quant_enfermeiros")]
+y_2 <- dados_trat[c("quant_enfermeiros")]
 
-X <- dados[!names(dados) %in% c("tempo_internacao", "quant_enfermeiros")]
+X <- dados_trat[!names(dados_trat) %in% c("tempo_internacao", "quant_enfermeiros")]
+
+# Correlação das variáveis
+(corr <- cor(dados[-1]))
+
+# Boxplots por variáveis categóricas
+dados %>% ggplot(aes(group = filiacao_escola_medicina, y = tempo_internacao)) +
+    geom_boxplot()
+dados %>% ggplot(aes(group = regiao, y = tempo_internacao)) +
+    geom_boxplot()
+
+dados %>% ggplot(aes(group = filiacao_escola_medicina, y = quant_enfermeiros)) +
+    geom_boxplot()
+dados %>% ggplot(aes(group = regiao, y = quant_enfermeiros)) +
+    geom_boxplot()
