7dbc218d43b1fbd5f693aab0ee239d0436475e46
Rafael Lira <rafaelvazlira@gmail.com>
2023-12-21 16:42:45 -0300

    Normalidade da primeira hipótese

--- main.R
+++ main.R
@@ -1,18 +1,19 @@
-pacman::p_load(readxl, tidyverse, ggplot2)
+pacman::p_load(readxl, tidyverse, ggplot2, MASS, DescTools, lmtest)
 
 # Importação de dados e renomeação de variáveis
-dados <- read_excel("./Dados/Dados_trabalho_20232.xlsx")
+dados <- read_excel("./Dados/Dados_trabalho_20232.xlsx")[-1]
 
-names(dados) <- c("id", "tempo_internacao", "idade", "prob_infeccao", "prop_culturas_rotina", "prop_raio_x_torax_rotina", "quant_leitos", "filiacao_escola_medicina", "regiao", "media_pacientes", "quant_enfermeiros", "servicos_disponiveis")
+names(dados) <- c(
+    "tempo_internacao", "idade", "prob_infeccao", "prop_culturas_rotina",
+    "prop_raio_x_torax_rotina", "quant_leitos", "filiacao_escola_medicina", "regiao",
+    "media_pacientes", "quant_enfermeiros", "servicos_disponiveis"
+)
 
 # Tratamento de variáveis categóricas
 dados_trat <- dados %>%
     mutate(
-        filiacao_escola_medicina = filiacao_escola_medicina == 1,
-        regiao_ne = regiao == 1,
-        regiao_nc = regiao == 2,
-        regiao_s = regiao == 3,
-        regiao = NULL
+        filiacao_escola_medicina = as.factor(filiacao_escola_medicina),
+        regiao = as.factor(regiao)
     )
 
 # Separando o banco em dados de treino e de validação
@@ -30,9 +31,6 @@ y_2 <- dados_treino[c("quant_enfermeiros")]
 
 X <- dados_treino[!names(dados_treino) %in% c("tempo_internacao", "quant_enfermeiros")]
 
-# Correlação das variáveis
-(corr <- cor(dados_treino[-1]))
-
 # Boxplots por variáveis categóricas
 dados_treino %>% ggplot(aes(group = filiacao_escola_medicina, y = tempo_internacao)) +
     geom_boxplot()
@@ -43,3 +41,15 @@ dados_treino %>% ggplot(aes(group = filiacao_escola_medicina, y = quant_enfermei
     geom_boxplot()
 dados_treino %>% ggplot(aes(group = regiao, y = quant_enfermeiros)) +
     geom_boxplot()
+
+
+modelo0 <- lm(log(quant_enfermeiros) ~ ., data = dados_treino %>% mutate(tempo_internacao = NULL))
+VIF(modelo0)
+
+modelo0 <- lm(log(quant_enfermeiros) ~ ., data = dados_treino %>% mutate(tempo_internacao = NULL, quant_leitos = NULL))
+VIF(modelo0)
+
+modelo0 <- step(modelo0, direction = "both")
+summary(modelo0)
+shapiro.test(modelo0$residuals)
+bptest(modelo0)
