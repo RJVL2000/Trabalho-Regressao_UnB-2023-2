6a61e4a5bc0d8cf933cb721a2774a30a09992c07
tailineju <190038144@aluno.unb.br>
2023-12-22 13:49:00 -0300

    update final?

--- main.R
+++ main.R
@@ -1,4 +1,5 @@
-pacman::p_load(readxl, tidyverse, ggplot2, MASS, DescTools, lmtest)
+if (!require(pacman)) install.packages("pacman")
+pacman::p_load(tidyverse,readxl,GGally,knitr,MASS,lmtest,DescTools)
 
 # Importação de dados e renomeação de variáveis
 dados <- read_excel("./Dados/Dados_trabalho_20232.xlsx")[-1]
@@ -13,8 +14,9 @@ names(dados) <- c(
 dados_trat <- dados %>%
     mutate(
         filiacao_escola_medicina = as.factor(filiacao_escola_medicina),
-        regiao = as.factor(regiao)
-    )
+        regiao = as.factor(regiao),
+        servicos_disponiveis2 = servicos_disponiveis^2
+        )
 
 # Separando o banco em dados de treino e de validação
 set.seed(2023)
@@ -24,82 +26,113 @@ index_valid <- indexes[!(indexes %in% index_treino)]
 dados_treino <- dados_trat[index_treino, ]
 dados_valid <- dados_trat[index_valid, ]
 
-# Separação em variáveis respostas e explicativas
-y_1 <- dados_treino[c("tempo_internacao")]
-
-y_2 <- dados_treino[c("quant_enfermeiros")]
-
-X <- dados_treino[!names(dados_treino) %in% c("tempo_internacao", "quant_enfermeiros")]
-
 # Boxplots por variáveis categóricas
-dados_treino %>% ggplot(aes(group = filiacao_escola_medicina, y = tempo_internacao)) +
-    geom_boxplot()
-dados_treino %>% ggplot(aes(group = regiao, y = tempo_internacao)) +
-    geom_boxplot()
 
-dados_treino %>% ggplot(aes(group = filiacao_escola_medicina, y = quant_enfermeiros)) +
-    geom_boxplot()
-dados_treino %>% ggplot(aes(group = regiao, y = quant_enfermeiros)) +
-    geom_boxplot()
+dados_treino %>% ggplot(aes(x = filiacao_escola_medicina, y = tempo_internacao)) +
+    geom_boxplot(fill='blue')+
+    xlab('Filiação Escola de Medicina')+
+    ylab('Tempo de Internação')+
+    theme_classic()
 
-# Regressão para explicar a quantidade de enfermeiros
-modelo0 <- lm(log(quant_enfermeiros) ~ servicos_disponiveis**2 + regiao, data = dados_treino %>% mutate(
-    tempo_internacao = NULL
-))
+dados_treino %>% ggplot(aes(x = regiao, y = tempo_internacao)) +
+    geom_boxplot(fill='blue')+
+    xlab('Região')+
+    ylab('Tempo de Internação')+
+    theme_classic()
 
-modelo0 <- step(modelo0, direction = "both")
-summary(modelo0)
+dados_treino %>% ggplot(aes(x = filiacao_escola_medicina, y = quant_enfermeiros)) +
+    geom_boxplot(fill='blue')+
+    xlab('Filiação Escola de Medicina')+
+    ylab('Quantidade de Enfermeiros')+
+    theme_classic()
 
-# Teste dos pressupostos para o primeiro modelo
-residuos0 <- modelo0$residuals
-shapiro.test(residuos0)
-plot(residuos0)
-bptest(modelo0)
+dados_treino %>% ggplot(aes(x = regiao, y = quant_enfermeiros)) +
+    geom_boxplot(fill='blue')+
+    xlab('Região')+
+    ylab('Quantidade de Enfermeiros')+
+    theme_classic()
+
+
+# --------------------------------------
+
+## Hipotose 1 - Y= quant_enfermeiros
+
+# Modelo 1 - todas, sem trasnformação
+modelo1 <- lm(quant_enfermeiros ~ servicos_disponiveis2 + regiao, dados_treino)
+summary(modelo1)
+
+residuos1 <- modelo1$residuals
+plot(residuos1)
+shapiro.test(residuos1) #nao atendido
+bptest(modelo1) #nao atendido
+
+# Modelo 2 - todas, log
+modelo2 <- lm(log(quant_enfermeiros) ~ servicos_disponiveis2 + regiao, dados_treino)
+summary(modelo2)
+
+residuos2 <- modelo2$residuals
+plot(residuos2)
+shapiro.test(residuos2) #atendido
+bptest(modelo2) #atendido
+
+# Modelo 3 - servicos, log
+modelo3 <- lm(log(quant_enfermeiros) ~ servicos_disponiveis2, dados_treino)
+summary(modelo3)
+
+residuos3 <- modelo3$residuals
+plot(residuos3)
+shapiro.test(residuos3) #atendido
+bptest(modelo3) #atendido
 
 # Medidas de ajuste do primeiro modelo
-rse0 <- sigma(modelo0)
-rmse0 <- sqrt(mean(modelo0$residuals^2))
-rsquared0 <- summary(modelo0)$r.squared
-adjusted_rsquared0 <- summary(modelo0)$adj.r.squared
-f_statistic0 <- summary(modelo0)$fstatistic[1]
-aic0 <- AIC(modelo0)
-bic0 <- BIC(modelo0)
+rse3 <- sigma(modelo3)
+rmse3 <- sqrt(mean(modelo3$residuals^2))
+rsquared3 <- summary(modelo3)$r.squared
+adjusted_rsquared3 <- summary(modelo3)$adj.r.squared
+f_statistic3 <- summary(modelo3)$fstatistic[1]
+aic3 <- AIC(modelo3)
+bic3 <- BIC(modelo3)
 
+medidas<-data.frame(rse3,rmse3,rsquared3,adjusted_rsquared3,f_statistic3,aic3,bic3)
+names(medidas) <- c('RSE','RMSE','R2','R2ad','F','AIC','BIC')
+kable(medidas)
 
-# Regressão para explicar o tempo de internação
-modelo1 <- lm(log(tempo_internacao) ~ ., data = dados_treino)
-VIF(modelo1)
+# --------------------------------------
+
+# Hipotese 2 : Y = tempo_internacao
+modelo12 <- lm(log(tempo_internacao) ~ ., data = dados_treino)
+VIF(modelo12)
 
 ## Remoção de variáveis correlacionadas
-modelo1 <- lm(log(tempo_internacao) ~ ., data = dados_treino %>% mutate(
+modelo22 <- lm(log(tempo_internacao) ~ ., data = dados_treino %>% mutate(
     quant_leitos = NULL,
     media_pacientes = NULL,
     servicos_disponiveis = NULL,
     prob_infeccao = NULL
 ))
-VIF(modelo1)
+VIF(modelo22)
 
-modelo1 <- step(modelo1, direction = "both")
-summary(modelo1)
+modelo32 <- step(modelo22, direction = "both")
+summary(modelo32)
 
 # Teste dos pressupostos para o segundo modelo
-residuos1 <- modelo1$residuals
-shapiro.test(residuos1)
-plot(residuos1)
-bptest(modelo1)
+residuos32 <- modelo32$residuals
+shapiro.test(residuos32)
+plot(residuos32)
+bptest(modelo32)
 
 # Medidas de ajuste do segundo modelo
-rse1 <- sigma(modelo1)
-rmse1 <- sqrt(mean(modelo1$residuals^2))
-rsquared1 <- summary(modelo1)$r.squared
-adjusted_rsquared1 <- summary(modelo1)$adj.r.squared
-f_statistic1 <- summary(modelo1)$fstatistic[1]
-aic1 <- AIC(modelo1)
-bic1 <- BIC(modelo1)
+rse32 <- sigma(modelo32)
+rmse32 <- sqrt(mean(modelo32$residuals^2))
+rsquared32 <- summary(modelo32)$r.squared
+adjusted_rsquared32 <- summary(modelo32)$adj.r.squared
+f_statistic32 <- summary(modelo32)$fstatistic[1]
+aic32 <- AIC(modelo32)
+bic32 <- BIC(modelo32)
 
 
 # Gerando previsões para os dados de validação
-prev_tempo_internacao <- exp(predict(modelo1, newdata = dados_valid))
+prev_tempo_internacao <- exp(predict(modelo32, newdata = dados_valid))
 res_valid <- dados_valid$tempo_internacao - prev_tempo_internacao
 rmse_valid <- sqrt(mean(res_valid))
 shapiro.test(res_valid)
